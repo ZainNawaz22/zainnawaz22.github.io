c29fc72e0c748223601973cbc40642fc71fc6511
Zain <i221085@nu.edu.pk>
2026-04-11 16:49:27 +0500

    feat: Add cursor spotlight effect with dynamic positioning and visibility control

--- scripts/effects/cursor-follower.js
+++ scripts/effects/cursor-follower.js
@@ -1,6 +1,7 @@
 export class CursorFollower {
     constructor(element) {
         this.element = element;
+        this.spotlight = document.querySelector('.cursor-spotlight');
         this.mouseX = 0;
         this.mouseY = 0;
         this.cursorX = 0;
@@ -30,19 +31,25 @@ export class CursorFollower {
     handleMouseMove(event) {
         this.mouseX = event.clientX;
         this.mouseY = event.clientY;
+        if (this.spotlight) {
+            this.spotlight.style.setProperty('--spotlight-x', `${event.clientX}px`);
+            this.spotlight.style.setProperty('--spotlight-y', `${event.clientY}px`);
+        }
     }
 
     show() {
         this.element.classList.add("visible");
+        if (this.spotlight) this.spotlight.classList.add("visible");
     }
 
     hide() {
         this.element.classList.remove("visible");
+        if (this.spotlight) this.spotlight.classList.remove("visible");
     }
 
     animate() {
-        this.cursorX += (this.mouseX - this.cursorX) * 0.12;
-        this.cursorY += (this.mouseY - this.cursorY) * 0.12;
+        this.cursorX += (this.mouseX - this.cursorX) * 0.22;
+        this.cursorY += (this.mouseY - this.cursorY) * 0.22;
         this.element.style.transform = `translate(${this.cursorX - 10}px, ${this.cursorY - 10}px)`;
         this.animationFrameId = window.requestAnimationFrame(() => this.animate());
     }
